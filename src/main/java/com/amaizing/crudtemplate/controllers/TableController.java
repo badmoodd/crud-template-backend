19db9e174942d86ac5622de53f506de82aa36b48
Egor Vasilkov <valoshka2003@gmail.com>
2024-04-29 16:08:56 +0300

    quick-fix: routes naming

--- src/main/java/com/amaizing/crudtemplate/controllers/TableController.java
+++ src/main/java/com/amaizing/crudtemplate/controllers/TableController.java
@@ -21,14 +21,14 @@ public class TableController {
         List<TableEntry> tables = new ArrayList<>();
 
         tables.add(new TableEntry("Body Types", "/api/v1/tables/body_types", "body_types"));
-        tables.add(new TableEntry("Car Models", "/api/v1/tables/car_model", "car_model"));
+        tables.add(new TableEntry("Car Models", "/api/v1/tables/car_models", "car_models"));
         tables.add(new TableEntry("Colors", "/api/v1/tables/colors", "colors"));
         tables.add(new TableEntry("Employees", "/api/v1/tables/employees", "employees"));
         tables.add(new TableEntry("Engines", "/api/v1/tables/engines", "engines"));
         tables.add(new TableEntry("Manufacturers", "/api/v1/tables/manufacturers", "manufacturers"));
         tables.add(new TableEntry("Sales", "/api/v1/tables/sales", "sales"));
         tables.add(new TableEntry("Service Centers", "/api/v1/tables/service_centers", "service_centers"));
-        tables.add(new TableEntry("Service Work Types", "api/v1/tables/service_work_types", "service_work_types"));
+        tables.add(new TableEntry("Service Work Types", "/api/v1/tables/service_work_types", "service_work_types"));
         tables.add(new TableEntry("Trims", "/api/v1/tables/trims", "trims"));
 
         return tables;
